9a9fb724509ba213df64f3d1dde7e409b9f26765
catru2 <camachotrujillomartinx@gmail.com>
2024-04-11 00:25:54 -0600

    avance

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -4,26 +4,32 @@
         <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
     </head>
     <body>
-        <div>
-            <h1>ASIT</h1>
-            <h1>Mantenimiento Seguro</h1>
-        </div>
+        <div class="contenedor">
+            <div class="primero">
+            <img src="/images/asitw.png" alt="Logotipo" class="logotipo">  
+            </div>    
         @if (session('error'))
             <div style="color: red">
                 {{ session('error') }}
             </div>
 
         @endif
-        <form action="{{ route('loginA') }}" method="POST">
+        <div class="segundo">
+        <form action="{{ route('loginA') }}" method="POST" class="formulario">
             @csrf
-            <label>Usuario</label>
-            <input type="text" name="username" placeholder="ingrese su Usuario">
-            <label>Password</label>
-            <input type="password" name="password" placeholder="ingrese su password">
-            <button type="submit">Entrar</button>
-        </form>
-
-
+            <div>
+            <h2>ASIT</h2>
+            <h2>Mantenimiento Seguro</h2>
+            <h1>ADMINISTRADOR</h1>
+            </div>
+            <label>Usuario:</label>
+            <input type="text" name="username" class="campo">
+            <label>Contraseña:</label>
+            <input type="password" name="password" class="campo">
+            <button type="submit" class="boton">Entrar</button>
+        </form> 
+        </div> 
+        </div>
     </body>
 
 
--- resources/views/admin/normaladmin/index.blade.php
+++ resources/views/admin/normaladmin/index.blade.php
@@ -1,17 +1,79 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href={{ asset('css/adminormal/style.css') }}>
+  <title>Admin</title>
 </head>
 <body>
-    <form action="{{ route('logoutA') }}" method="POST">
-        @csrf
-        <button type="submit">Logout</button>
-        </form>
-    <h1>normal admin</h1>
 
+<nav style="background-color: #1F1F29" class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand">
+        <img src="/images/asitw.png" alt="ASIT" width="80" height="45">
+      </a>
+      <div class="mx-auto">
+        <h1 class="navbar-text title">Equipos en servicio</h1>
+      </div>
+      <div class="d-flex gap-3">
+        <form action="{{ route('logoutA') }}" method="POST">
+          @csrf
+          <button type="submit" class="btn btn_blue" style="margin">SALIR</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+  <div class="col">
+    <div class="card h-100 w-50">
+      <img src="/images/ejemplo.jpg" class="card-img-top" alt="..."  width="100" height="120">
+      <div class="card-body">
+        <h5 class="card-title">ID: XXXXXX</h5>
+        <p class="card-text">Estatus: En mantenimiento</p>
+      </div>
+      <div class="card-footer">
+      <div class="contendor-botones">
+      <button type="button" class="btn-btn-sm-1 ">Comentar</button>
+      <button type="button" class="btn-btn-sm-2 ">Actualizar</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 w-50">
+      <img src="/images/ejemplo.jpg" class="card-img-top" alt="..."  width="100" height="120">
+      <div class="card-body">
+        <h5 class="card-title">ID: XXXXXX</h5>
+        <p class="card-text">Estatus: En fila</p>
+      </div>
+      <div class="card-footer">
+      <div class="contendor-botones">
+      <button type="button" class="btn-btn-sm-1 ">Comentar</button>
+      <button type="button" class="btn-btn-sm-2 ">Actualizar</button>
+      </div>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100 w-50">
+      <img src="/images/ejemplo.jpg" class="card-img-top" alt="..."  width="100" height="120">
+      <div class="card-body">
+        <h5 class="card-title">ID: XXXXXX</h5>
+        <p class="card-text">Estatus: Lista</p>
+      </div>
+      <div class="card-footer">
+      <div class="contendor-botones">
+      <button type="button" class="btn-btn-sm-1 ">Comentar</button>
+      <button type="button" class="btn-btn-sm-2 ">Actualizar</button>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
 </body>
 </html>
