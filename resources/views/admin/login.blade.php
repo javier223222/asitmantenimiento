36cb5bf1d56b48aa06c386b18d7996c31b878193
Alex Esquinca <viniboygamers>
2024-04-11 11:03:22 -0600

    cambios

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,30 +1,31 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
-    </head>
-    <body>
-        <div>
-            <h1>ASIT</h1>
-            <h1>Mantenimiento Seguro</h1>
+
+<head>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
+</head>
+
+<body>
+    <div>
+        <h1>ASIT</h1>
+        <h1>Mantenimiento Seguro</h1>
+    </div>
+    @if (session('error'))
+        <div style="color: red">
+            {{ session('error') }}
         </div>
-        @if (session('error'))
-            <div style="color: red">
-                {{ session('error') }}
-            </div>
-
-        @endif
-        <form action="{{ route('loginA') }}" method="POST">
-            @csrf
-            <label>Usuario</label>
-            <input type="text" name="username" placeholder="ingrese su Usuario">
-            <label>Password</label>
-            <input type="password" name="password" placeholder="ingrese su password">
-            <button type="submit">Entrar</button>
-        </form>
+    @endif
+    <form action="{{ route('loginA') }}" method="POST">
+        @csrf
+        <label>Usuario</label>
+        <input type="text" name="username" placeholder="ingrese su Usuario">
+        <label>Password</label>
+        <input type="password" name="password" placeholder="ingrese su password">
+        <button type="submit">Entrar</button>
+    </form>
 
 
-    </body>
+</body>
 
 
 </html>
--- resources/views/admin/superadmin/index.blade.php
+++ resources/views/admin/superadmin/index.blade.php
@@ -1,113 +1,135 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-    <link rel="stylesheet" href={{ asset('css/admin/style.css') }}>
-    <title>Super Admin</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href={{ asset('css/admin/style.css') }}>
+  <title>Super Admin</title>
 </head>
+
 <body>
-
-
-
-    <nav style="background-color: #1F1F29" class="navbar bg-body-tertiary">
-        <div style="background-color: #1F1F29" class="container">
-            <form action="{{ route('logoutA') }}" method="POST">
-                @csrf
-                <button type="submit">Cerrar Sesion</button>
-                </form>
+  <nav style="background-color: #1F1F29" class="navbar navbar-expand-lg">
+    <div class="container">
+      <a class="navbar-brand">
+        <img src="/images/asitw.png" alt="ASIT" width="90" height="50">
+      </a>
+      <div class="mx-auto">
+        <h1 class="navbar-text title">Técnicos y equipos</h1>
+      </div>
+      <div class="d-flex gap-3">
+        <div>
+          <a href={{ route('addTecnico') }} class="btn btn-light">Agregar técnico</a>
         </div>
-      </nav>
-
-
-      <form action={{route("searchTecnico")}} method="POST" class="input-group">
-        @csrf
-        <input type="search" name="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
-        <button  class="btn btn-outline-primary">Buscar</button>
-      </form>
-
-
-<div>
-    <a href={{route("addTecnico")}} class="btn btn-primary">Agregar tecnico</a>
-
-</div>
-@if ($isSearch)
-<div>
-    <a href={{route("superadmin")}} class="btn btn-primary">Ver a todos los tecnicos</a>
-
-</div>
-
-@endif
-
-
-    @if (empty($admins[0]))
-        <h1>no hay tecnicos</h1>
-    @else
-     <div class="row">
-      @foreach ($admins as $item)
-      <div class="col-sm-6 mb-3 mb-sm-0">
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-              <h5 class="card-title">Tenico</h5>
-              <h6 class="card-subtitle mb-2 text-body-secondary">Usuario: {{$item -> username}}</h6>
-              <h6 class="card-subtitle mb-2 text-body-secondary">Nombre: {{$item->name}} {{$item->last_name}} {{$item->mother_last_name}}</h6>
-
-              <p class="card-text">Contrasenia: {{$item->password_text}}</p>
-              <h6 class="card-subtitle mb-2 text-body-secondary">Creado: {{date("Y-m-d",strtotime(date($item->created_at)))}}</h6>
-              <form action={{route("deleteTecnico",["id"=>$item->id])}} method="POST">
-                @csrf
-                @method("DELETE")
-
-                <button class="btn btn-danger">Eliminar<i class="bi bi-trash"></i></button>
-
-              </form>
-
-
-              <a href={{route("updateTecnico",["id"=>$item->id])}} class="btn btn-primary">Actualizar</a>
-
-            </div>
-          </div>
+        <form action="{{ route('logoutA') }}" method="POST">
+          @csrf
+          <button type="submit" class="btn btn_blue" style="margin">SALIR</button>
+        </form>
+      </div>
     </div>
-      @endforeach
+  </nav>
+
+
+  <form action={{ route('searchTecnico') }} method="POST" class="input-group">
+    @csrf
+    <div class="container-fluid px-4 text-justify">
+      <div class="col-sm-2">
+        <div class="input-group my-3">
+          <input type="search" name="search" class="form-control" placeholder="Buscar técnico" aria-label="Search"
+            aria-describedby="button-addon2">
+          <button class="btn btn-outline-light">Buscar</button>
+        </div>
+      </div>
+    </div>
+  </form>
+
+
+
+
+  @if ($isSearch)
+    <div class="container-fluid px-4 text-justify">
+      <div>
+        <a href={{ route('superadmin') }} class="btn btn-outline-light">Ver a todos los tecnicos</a>
 
       </div>
-</div>
+    </div>
+  @endif
 
 
-     @endif
+  @if (empty($admins[0]))
+    <div class="container-fluid px-4 text-justify">
+      <h1 class="title">Técnico no encontrado</h1>
+    </div>
+  @else
+    <div class="container-fluid px-4 text-justify">
+      <div class="row gy-3">
+        @foreach ($admins as $item)
+          <div class="col-5 d-flex justify-content-start">
+            <div class="card" style="width: 17vw">
+              <div class="card-body">
+                <h5 class="card-title">Técnico</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Usuario: {{ $item->username }}</h6>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Nombre: {{ $item->name }}
+                  {{ $item->last_name }} {{ $item->mother_last_name }}</h6>
 
-     @if (empty($admins[0]))
+                <p class="card-text">Contraseña: {{ $item->password_text }}</p>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Creado:
+                  {{ date('Y-m-d', strtotime(date($item->created_at))) }}</h6>
 
-     @else
-     <div class="container">
-        <div class="row">
+                <div class="row">
+                  <div class="col">
+                    <form action={{ route('deleteTecnico', ['id' => $item->id]) }} method="POST">
+                      @csrf
+                      @method('DELETE')
 
-     @if ($totalpages>1)
+                      <button class="btn btn-danger">Eliminar <i class="bi bi-trash"></i></button>
 
-    @for ($i = 1; $i <= $totalpages; $i++)
+                    </form>
+                  </div>
+                  <div class="col">
+                    <a href={{ route('updateTecnico', ['id' => $item->id]) }} class="btn btn-primary">Actualizar</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        @endforeach
+        
 
-
-    <div class="col-sm">
-
-        <a href={{route("superadmin",["page"=>$i])}}>{{$i}}</a>
-
-
+      </div>
     </div>
 
 
-@endfor
+  @endif
 
-     @endif
+  @if (empty($admins[0]))
+  @else
+    <div class="container">
+      <div class="row">
 
-</div>
+        @if ($totalpages > 1)
 
-     @endif
+          @for ($i = 1; $i <= $totalpages; $i++)
+            <div class="col-sm">
 
-</div>
+              <a href={{ route('superadmin', ['page' => $i]) }}>{{ $i }}</a>
+
+
+            </div>
+          @endfor
+
+        @endif
+
+      </div>
+
+  @endif
+
+  </div>
 
 
 </body>
+
 </html>
